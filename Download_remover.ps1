88561136fd7e48ae8973383615cf8f79c2dc0ee4
Trent Schake <trent_schake2005@outlook.com>
2023-03-11 14:35:46 -0600

    Create Download_remover.ps1

---
+++ Download_remover.ps1
@@ -0,0 +1,28 @@
+﻿# Script to automate deletion of files within the downloads folder
+# Version 1.0
+# Written by Dendrite
+
+# Setting task action
+New-SceduledTaskAction -Execute 'Download_remover.ps1'
+# Setting task trigger
+New-ScheduledTaskTrigger -DaysOfWeek Saturday -At 12pm
+
+    
+# Declaring main
+function main
+{
+    # Variable for path of files within downloads folder
+    $DownloadFolder = C:\Users\$env:USERNAME\Downloads\*
+
+
+    # removes the files from the downloads folder
+    remove-item $DownloadFolder
+    
+    # Clears the recyclebin
+    Clear-RecycleBin
+}
+
+
+
+# Calls main
+main
